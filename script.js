b8f5078aeefbb57596f7ff945429c2ad3cde3693
Eltfvzh <148296570+Eltfvzh@users.noreply.github.com>
2023-11-01 16:59:17 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,35 +13,24 @@ tombol.addEventListener("click", function(){
     h1.innerHTML = "ada pesan buat kamu <3";
     lanjut.innerHTML = "lanjut?";
     tombol.style.display = "none";
-    lanjut1.style = "none";
 
     lanjut.addEventListener("click", function(){
         lanjut.style.display = "none";
         tombol.style.display = "none";
         h1.style.display = "none";
-        p.innerHTML = "Kamu tuh lucu banget sii apalagi pas senyum hehe... cuma kamu yang bisa buat aku senyum senyum sendiri di kelas kamu itu penyemangat sekolah bagi aku, ngeliat kamu sekilas aja udh bikin aku seneng tau, aku salting terus setiap ngeliat kamu, saking manisnya";
-        lanjut1.innerHTML = "lanjut lagi?";
+        p.innerHTML = "disini aku cuma mau ngungkapin perasaan aku, masalah aku diterima atau ngak itu tergantung kamunya, aku juga ngak bakal berharap lebih kok, kalo kamu risih bilang aja ya, BTW i have crush on you.. , aku nyatain ini biar hati aku lega aja karna udah ngungkapin perasaaan ini ke kamu, kalau semisalnya kamu udah punya pacar maaf yaa, karna udah confess ke kamu, segitu aja ya.. aku tunggu";
+        lanjut2.innerHTML = "Mau";
+        lanjut3.innerHTML = "Ngak";
 
-        lanjut1.addEventListener("click", function(){
-            lanjut.style.display = "none";
-            tombol.style.display = "none";
-            h1.style.display = "none";
-            p.style.display = "none";
-            lanjut1.style.display = "none";
-            lanjut2.innerHTML = "Mau";
-            lanjut3.innerHTML = "Ngak";
-            h4.innerHTML =  "disini aku cuma mau ngungkapin perasaan aku, masalah aku diterima atau ngak itu tergantung kamunya, aku juga ngak bakal berharap lebih kok, kalo kamu risih bilang aja ya, BTW i have crush on you.. , aku nyatain ini biar hati aku lega aja karna udah ngungkapin perasaaan ini ke kamu, kalau semisalnya kamu udah punya pacar maaf yaa, karna udah confess ke kamu, segitu aja ya.. ku tunggu"
+        lanjut3.addEventListener("click", function(){
+            let hasil = confirm("beneran");
 
-            lanjut3.addEventListener("click", function(){
-                let hasil = confirm("beneran");
-
-                if (hasil == true){
-                    alert("makasih yaa udah mau dengerin");
-                }else{
-                    alert("yehh yang bener dongg")
-                }
-            })
-
+            if (hasil == true){
+                alert("makasih yaa udah mau dengerin");
+            }else{
+                alert("yehh yang bener dongg")
+            }
         })
+
     })
 })
